9e261cd0be88a69fb4ddf43c2df3af50d45f66c5
Octavic <yifeixu@live.com>
2017-08-18 17:26:44 -0700

    Initial push
    - Add basic unity scene with 1 ship
    - Add basic ship script with color control

---
+++ SpaceRPG/Assets/Scripts/Ship.cs
@@ -0,0 +1,61 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="Ship.cs">
+//    Copyright (c) Yifei Xu .  All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace Assets.Scripts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+    using Utility;
+
+    /// <summary>
+    /// Describes the basic functionality of a ship
+    /// </summary>
+	public class Ship : MonoBehaviour
+    {
+        /// <summary>
+        /// The main paintjob game object
+        /// </summary>
+        public GameObject MainPaintjob;
+
+        /// <summary>
+        /// Gets the main color of the ship
+        /// </summary>
+        public Color MainColor
+        {
+            get
+            {
+                return this._mainSpriteRenderer.color;
+            }
+            set
+            {
+                this._mainSpriteRenderer.color = value;
+            }
+        }
+
+        /// <summary>
+        /// The main sprite
+        /// </summary>
+        private SpriteRenderer _mainSpriteRenderer;
+        
+        /// <summary>
+        /// Called in the betginning
+        /// </summary>
+        private void Start()
+        {
+            this._mainSpriteRenderer = this.MainPaintjob.GetComponent<SpriteRenderer>();
+        }
+
+        /// <summary>
+        /// Called once per frame
+        /// </summary>
+        private void Update()
+        {
+        }
+    }
+}
